f0d37db9d1379e4199b5213ffedaf6e6e3f9ca36
Andika PT <andikatampubolon72@gmail.com>
2025-03-03 16:39:14 +0700

    migration mcr

--- Computer-Based-Test-Website/routes/auth.php
+++ Computer-Based-Test-Website/routes/auth.php
@@ -1,25 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-<<<<<<< HEAD
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('guest')->group(function () {    
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-=======
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
@@ -75,5 +56,4 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
->>>>>>> 27ad573461cbc468f7e72f20298adef52d4924f4
 });
--- Computer-Based-Test-Website/routes/web.php
+++ Computer-Based-Test-Website/routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-<<<<<<< HEAD
-use App\Http\Controllers\OperatorController;
-=======
->>>>>>> 27ad573461cbc468f7e72f20298adef52d4924f4
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\bisnisOperatorController;
@@ -13,27 +9,6 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-<<<<<<< HEAD
-Route::get('operators/create', [OperatorController::class, 'create'])->name('createOperator');
-Route::post('operators', [OperatorController::class, 'store'])->name('listOperator');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::prefix('admin')->name('admin.')->group(function () {
-        Route::get('operators', [OperatorController::class, 'index'])->name('listOperator');
-        Route::get('operators/create', [OperatorController::class, 'create'])->name('createOperator');
-        Route::post('operators', [OperatorController::class, 'store'])->name('storeOperator');
-        Route::get('operators/{operator}/edit', [OperatorController::class, 'edit'])->name('editOperator');
-        Route::put('operators/{operator}', [OperatorController::class, 'update'])->name('updateOperator');
-        Route::delete('operators/{operator}', [OperatorController::class, 'destroy'])->name('deleteOperator');
-        Route::get('/bisnis-dashboard', [BisnisOperatorController::class, 'index'])->name('bisnisDashboard');
-        Route::get('/bisnis/create', [BisnisOperatorController::class, 'create'])->name('createBisnis');
-        Route::post('/bisnis/store', [BisnisOperatorController::class, 'store'])->name('storeBisnis');
-        Route::delete('/bisnis/{bisnis}', [BisnisOperatorController::class, 'destroy'])->name('bisnis.destroy');
-    });
-});
-
-require __DIR__.'/auth.php';
-=======
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -45,4 +20,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
->>>>>>> 27ad573461cbc468f7e72f20298adef52d4924f4
