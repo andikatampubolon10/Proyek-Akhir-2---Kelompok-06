7ebb9a4119ffdc7c10f43628cd8939f79ca7d364
Andika PT <andikatampubolon72@gmail.com>
2025-03-03 16:06:00 +0700

    Update RolePermissionSeeder.php

--- Computer-Based-Test-Website/database/seeders/RolePermissionSeeder.php
+++ Computer-Based-Test-Website/database/seeders/RolePermissionSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Validation\Rules\Email;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -12,6 +16,45 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $permissions = [
+            'view courses',
+            'create courses',
+            'edit courses',
+            'delete courses',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create([
+                'name' => $permission
+            ]);
+        }
+
+        $teacherRole = Role::create([
+            'name' => 'teacher'
+        ]);
+
+        $teacherRole->givePermissionTo([
+            'view courses',
+            'create courses',
+            'edit courses',
+            'delete courses',
+        ]);
+
+        $studentRole = Role::create([
+            'name' => 'student'
+        ]);
+
+        $studentRole->givePermissionTo([
+            'view courses',
+        ]);
+
+        // membuat data super admin
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $user->assignRole($teacherRole);
     }
 }
